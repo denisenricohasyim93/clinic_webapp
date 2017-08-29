5d97604587d104bd51c3b5afaeaceebf36ff478f
Alan Dabagh <alan.dabagh@gmail.com>
2017-08-29 08:38:20 +0200

    nothing serious

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -4,15 +4,7 @@ class Home extends Component {
     render() {
         return (
             <div className="route_section" id="home_route">
-                <h2>To build:</h2>
-                <br />
-                <ul>
-                    <li>Recent Activities (if admin: show recent activities for all users in the clinic)</li>
-                    <li>Near upcoming appointments</li>
-                    <li>Notifications for events (can be customized in settings)</li>
-                    <li>Time and date</li>
-                    <li>Add note functionality here => (new user) or (existing)</li>
-                </ul>
+                <h2>Demo Clinic Web System</h2>
             </div>
         );
     }
