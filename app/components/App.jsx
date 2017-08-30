4b4fc100b06fdd0d71d87161dcabff0ffbff3d3d
Alan Dabagh <alan.dabagh@gmail.com>
2017-08-30 22:23:07 +0200

    exporting functions

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,5 +1,18 @@
 import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
+import {
+    remove_selected_patient,
+    darken,
+    add_patient,
+    add_item,
+    navigate,
+    move_appointment,
+    add_appointment,
+    set_appointments,
+    add_dropdown_item,
+    stop_medicine,
+    show_patient_profile,
+} from './MainFunctions'
 
 import {
     BrowserRouter as Router,
@@ -28,6 +41,18 @@ class App extends Component {
             medicine_list: data.medicine_list,
             medicine_dose_list: data.medicine_dose_list
         }
+
+        this.darken = darken.bind(this)
+        this.remove_selected_patient = remove_selected_patient.bind(this)
+        this.add_item = add_item.bind(this)
+        this.add_patient = add_patient.bind(this)
+        this.navigate = navigate.bind(this)
+        this.move_appointment = move_appointment.bind(this)
+        this.add_appointment = add_appointment.bind(this)
+        this.set_appointments = set_appointments.bind(this)
+        this.add_dropdown_item = add_dropdown_item.bind(this)
+        this.stop_medicine = stop_medicine.bind(this)
+        this.show_patient_profile = show_patient_profile.bind(this)
     }
 
     componentDidMount() {
@@ -59,7 +84,7 @@ class App extends Component {
                                     patients={this.state.patients}
                                     add_patient={this.add_patient.bind(this)}
                                     add_appointment={this.add_appointment.bind(this)}
-                                    darken={this.darken.bind(this)}
+                                    darken={this.darken}
                                     add_item={this.add_item.bind(this)}
                                     selected_patient={this.state.selected_patient} />
                             } />
@@ -78,201 +103,6 @@ class App extends Component {
                 </Router>
             </div>
         );
-    }
-
-    remove_selected_patient() {
-        this.setState({ selected_patient: [] });
-    }
-
-    darken() {
-        let darken_div = document.querySelector(".darken")
-        darken_div.classList.toggle("darken_show");
-    }
-
-    add_patient(patient) {
-        if (patient.name &&
-            patient.date &&
-            patient.gender &&
-            patient.address) {
-
-            let updated_patients = this.state.patients.slice(),
-                birth = patient.date.split("-"),
-                reversed_birth = birth.reverse().join("-"),
-                date1 = new Date(),
-                date2 = new Date(patient.date),
-                time_diff = Math.abs(date2.getTime() - date1.getTime()),
-                diff_years = Math.ceil((time_diff / (1000 * 3600 * 24) / 365))
-
-            updated_patients.unshift({
-                name: patient.name,
-                birth: reversed_birth,
-                gender: patient.gender,
-                id: Math.floor(Math.random() * (999999 - 100000)) + 100000,
-                age: diff_years,
-                gravida: patient.gravida,
-                hypertension: patient.hypertension,
-                diabetes: patient.diabetes,
-                phone: patient.phone,
-                smoker: patient.smoker,
-                address: patient.address,
-                notes: [],
-                appointments: [],
-                vitals: [],
-                "medicine": [],
-                "diagnosis": []
-            })
-
-            this.setState({ patients: updated_patients });
-        }
-    }
-
-    add_item(item, patient, property) {
-        let patients = this.state.patients.slice()
-
-        for (let i = 0; i < patients.length; i++) {
-            if (patients[i].name === patient.name) {
-                patients[i][property].unshift(item);
-            }
-        }
-
-        this.setState({ patients: patients });
-    }
-
-    navigate(patient) {
-        let selected_patient,
-            patients = this.state.patients;
-
-        for (let i = 0; i < patients.length; i++) {
-            if (patients[i].name === patient) {
-                selected_patient = patients[i];
-            }
-        }
-
-        this.setState({ selected_patient: [selected_patient] });
-    }
-
-    move_appointment(event, start, end) {
-        let patients = this.state.patients.slice(),
-            appointments = [],
-            patient_index,
-            appointment_index,
-            appointment = {
-                "title": event.title,
-                "start": start,
-                "end": end,
-                "desc": event.desc
-            };
-
-        for (let i = 0; i < patients.length; i++) {
-            if (patients[i].name === event.title) {
-
-                for (let j = 0; j < patients[i].appointments.length; j++) {
-                    if (patients[i].appointments[j].start === event.start) {
-
-                        patient_index = i;
-                        appointment_index = j;
-                        patients[i].appointments.push(appointment);
-                        break;
-                    }
-                }
-            }
-        }
-
-
-        if (typeof (patient_index) === "number") {
-            patients[patient_index].appointments.splice(appointment_index, 1)
-        }
-
-        patients.map((patient) => {
-            patient.appointments.map((apt) => {
-                appointments.push(apt)
-            })
-        })
-
-        this.setState({
-            patients: patients,
-            events: appointments
-        })
-    }
-
-    add_appointment(appointment, patient) {
-        let patients = this.state.patients.slice(),
-            selected_patient,
-            appointments = [];
-
-        for (let i = 0; i < patients.length; i++) {
-            if (patients[i].name === patient) {
-                selected_patient = patients[i];
-                patients[i].appointments.unshift(appointment);
-            }
-        }
-
-        this.state.patients.map((patient) => {
-            patient.appointments.map((apt) => {
-                appointments.push(apt)
-            })
-        })
-
-
-        this.setState({
-            patients: patients,
-            events: appointments,
-            selected_patient: [selected_patient]
-        })
-    }
-
-    set_appointments() {
-        let appointments = []
-
-        this.state.patients.map((patient) => {
-            patient.appointments.map((apt) => {
-                appointments.push(apt)
-            })
-        })
-
-        this.setState({ events: appointments })
-    }
-
-    add_dropdown_item(item, category) {
-        let new_items = this.state[category].slice()
-        new_items.push(item)
-
-        if (category === "diagnosis_list") {
-            this.setState({ diagnosis_list: new_items })
-        }
-        if (category === "medicine_list") {
-            this.setState({ medicine_list: new_items })
-        }
-
-        if (category === "medicine_dose_list") {
-            this.setState({ medicine_dose_list: new_items })
-        }
-    }
-
-    stop_medicine(patient, medicine) {
-        let patients = this.state.patients.slice()
-
-        for (let i = 0; i < patients.length; i++) {
-            if (patients[i].name === patient.name) {
-
-                for (let j = 0; j < patients[i].medicine.length; j++) {
-                    if (patients[i].medicine[j] === medicine) {
-                        patients[i].medicine[j]["stopped"] = "stopped"
-                        break;
-                    }
-                }
-            }
-        }
-
-        this.setState({ patients: patients })
-    }
-
-    show_patient_profile(patient) {
-        this.setState({ selected_patient: [patient] })
-    }
-
-    remove_selected_patient() {
-        this.setState({ selected_patient: [] })
     }
 }
 
---
+++ app/components/MainFunctions.js
@@ -0,0 +1,208 @@
+export {
+    remove_selected_patient,
+    darken,
+    add_patient,
+    add_item,
+    navigate,
+    move_appointment,
+    add_appointment,
+    set_appointments,
+    add_dropdown_item,
+    stop_medicine,
+    show_patient_profile,
+}
+
+function remove_selected_patient() {
+    this.setState({ selected_patient: [] });
+}
+
+function darken() {
+    let darken_div = document.querySelector(".darken")
+    darken_div.classList.toggle("darken_show");
+}
+
+function add_patient(patient) {
+    if (patient.name &&
+        patient.date &&
+        patient.gender &&
+        patient.address) {
+
+        let updated_patients = this.state.patients.slice(),
+            birth = patient.date.split("-"),
+            reversed_birth = birth.reverse().join("-"),
+            date1 = new Date(),
+            date2 = new Date(patient.date),
+            time_diff = Math.abs(date2.getTime() - date1.getTime()),
+            diff_years = Math.ceil((time_diff / (1000 * 3600 * 24) / 365))
+
+        updated_patients.unshift({
+            name: patient.name,
+            birth: reversed_birth,
+            gender: patient.gender,
+            id: Math.floor(Math.random() * (999999 - 100000)) + 100000,
+            age: diff_years,
+            gravida: patient.gravida,
+            hypertension: patient.hypertension,
+            diabetes: patient.diabetes,
+            phone: patient.phone,
+            smoker: patient.smoker,
+            address: patient.address,
+            notes: [],
+            appointments: [],
+            vitals: [],
+            "medicine": [],
+            "diagnosis": []
+        })
+
+        this.setState({ patients: updated_patients });
+    }
+}
+
+function add_item(item, patient, property) {
+    let patients = this.state.patients.slice()
+
+    for (let i = 0; i < patients.length; i++) {
+        if (patients[i].name === patient.name) {
+            patients[i][property].unshift(item);
+        }
+    }
+
+    this.setState({ patients: patients });
+}
+
+function navigate(patient) {
+    let selected_patient,
+        patients = this.state.patients;
+
+    for (let i = 0; i < patients.length; i++) {
+        if (patients[i].name === patient) {
+            selected_patient = patients[i];
+        }
+    }
+
+    this.setState({ selected_patient: [selected_patient] });
+}
+
+function move_appointment(event, start, end) {
+    let patients = this.state.patients.slice(),
+        appointments = [],
+        patient_index,
+        appointment_index,
+        appointment = {
+            "title": event.title,
+            "start": start,
+            "end": end,
+            "desc": event.desc
+        };
+
+    for (let i = 0; i < patients.length; i++) {
+        if (patients[i].name === event.title) {
+
+            for (let j = 0; j < patients[i].appointments.length; j++) {
+                if (patients[i].appointments[j].start === event.start) {
+
+                    patient_index = i;
+                    appointment_index = j;
+                    patients[i].appointments.push(appointment);
+                    break;
+                }
+            }
+        }
+    }
+
+
+    if (typeof (patient_index) === "number") {
+        patients[patient_index].appointments.splice(appointment_index, 1)
+    }
+
+    patients.map((patient) => {
+        patient.appointments.map((apt) => {
+            appointments.push(apt)
+        })
+    })
+
+    this.setState({
+        patients: patients,
+        events: appointments
+    })
+}
+
+function add_appointment(appointment, patient) {
+    let patients = this.state.patients.slice(),
+        selected_patient,
+        appointments = [];
+
+    for (let i = 0; i < patients.length; i++) {
+        if (patients[i].name === patient) {
+            selected_patient = patients[i];
+            patients[i].appointments.unshift(appointment);
+        }
+    }
+
+    this.state.patients.map((patient) => {
+        patient.appointments.map((apt) => {
+            appointments.push(apt)
+        })
+    })
+
+
+    this.setState({
+        patients: patients,
+        events: appointments,
+        selected_patient: [selected_patient]
+    })
+}
+
+function set_appointments() {
+    let appointments = []
+
+    this.state.patients.map((patient) => {
+        patient.appointments.map((apt) => {
+            appointments.push(apt)
+        })
+    })
+
+    this.setState({ events: appointments })
+}
+
+function add_dropdown_item(item, category) {
+    let new_items = this.state[category].slice()
+    new_items.push(item)
+
+    if (category === "diagnosis_list") {
+        this.setState({ diagnosis_list: new_items })
+    }
+    if (category === "medicine_list") {
+        this.setState({ medicine_list: new_items })
+    }
+
+    if (category === "medicine_dose_list") {
+        this.setState({ medicine_dose_list: new_items })
+    }
+}
+
+function stop_medicine(patient, medicine) {
+    let patients = this.state.patients.slice()
+
+    for (let i = 0; i < patients.length; i++) {
+        if (patients[i].name === patient.name) {
+
+            for (let j = 0; j < patients[i].medicine.length; j++) {
+                if (patients[i].medicine[j] === medicine) {
+                    patients[i].medicine[j]["stopped"] = "stopped"
+                    break;
+                }
+            }
+        }
+    }
+
+    this.setState({ patients: patients })
+}
+
+function show_patient_profile(patient) {
+    this.setState({ selected_patient: [patient] })
+}
+
+function remove_selected_patient() {
+    this.setState({ selected_patient: [] })
+}
